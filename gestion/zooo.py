c17abd2d09b89c63bfc54fb1dbf1dd9dc3a4798a
Juan José García Vásquez <juagarciava@unal.edu.co>
2022-04-27 02:11:30 -0500

    :)

---
+++ gestion/zooo.py
@@ -0,0 +1,16 @@
+class Zoologico:
+    def __init__(self,nombre,ubicacion,zonas):
+        _nombre = nombre
+        _ubicacion = ubicacion
+        _zonas =zonas
+    def agregarZonas(self):
+    @classmethod
+    def cantidadAnimales(clc):
+class Zona(Zoologico):
+    def __init__(self, nombre, zoo, animales):
+        _nombre = nombre
+        _zoo=zoo
+        _animales = animales
+    def agregarAnimales(self):
+    @classmethod
+    def cantidadAnimales(clc):
